adf3a20b066e301afbd21d7428d8e34ece74843c
e-rick23 <erickmarques63@gmail.com>
2024-08-15 20:31:51 -0300

    [Erick] feat: Upload do main

--- src/main/java/br/ufrn/imd/Main.java
+++ src/main/java/br/ufrn/imd/Main.java
@@ -1,7 +1,18 @@
 package br.ufrn.imd;
 
+import br.ufrn.imd.Views.LoginForm;
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        //Login.login(); // Chama o método de login!
+
+        SwingUtilities.invokeLater(new Runnable(){
+            @Override
+            public void run(){
+                new LoginForm().setVisible(true);
+            }
+        });
     }
 }
