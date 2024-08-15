d9441740dbf5db7d07b986a94036b0e5bde8e1e3
e-rick23 <erickmarques63@gmail.com>
2024-08-15 20:19:36 -0300

    Adicionado cores base das telas

---
+++ src/main/java/br/ufrn/imd/constants/colors.java
@@ -0,0 +1,10 @@
+package br.ufrn.imd.constants;
+
+import java.awt.*;
+
+public class colors {
+    public static final Color PRIMARY_COLOR = Color.decode("#83C5BE");
+    public static final Color SECONDARY_COLOR = Color.decode("#EDF6F9");
+    public static final Color TERTIARY_COLOR = Color.decode("#FF5A5F");
+    public static final Color TEXT_COLOR = Color.decode("#006D77");
+}
