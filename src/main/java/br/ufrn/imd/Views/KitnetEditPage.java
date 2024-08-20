ca395433828e70b44efad38b2ffde49c546e7be8
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2024-08-20 10:42:16 -0300

    [Luiz] update: ajustando o layout da tela de editar kitnet

--- src/main/java/br/ufrn/imd/Views/KitnetEditPage.java
+++ src/main/java/br/ufrn/imd/Views/KitnetEditPage.java
@@ -75,23 +75,29 @@ public class KitnetEditPage extends MyFrame implements ActionListener {
             }
         });
 
+        JLabel loginLabel = new JLabel("Editar Kitnet");
+        loginLabel.setBounds(360, 25, 520, 100);
+        loginLabel.setForeground(Colors.TEXT_COLOR);
+        loginLabel.setFont(new Font("Dialog", Font.BOLD, 40));
+        loginLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(loginLabel);
 
         JLabel nKitnetLabel = new JLabel("N° Kitnet:");
-        nKitnetLabel.setBounds(30, 45, 200, 25);
+        nKitnetLabel.setBounds(410, 135, 400, 25);
         nKitnetLabel.setForeground(Colors.TEXT_COLOR);
         nKitnetLabel.setFont(new Font("Dialog", Font.PLAIN, 18));
 
-        nKitnetField.setBounds(30, 75, 400, 55);
+        nKitnetField.setBounds(410, 175, 220, 25);
         nKitnetField.setBackground(Colors.SECONDARY_COLOR);
         nKitnetField.setForeground(Colors.TEXT_COLOR);
         nKitnetField.setFont(new Font("Dialog", Font.PLAIN, 24));
 
         JLabel mobiliaLabel = new JLabel("Mobília:");
-        mobiliaLabel.setBounds(460, 45, 200, 25);
+        mobiliaLabel.setBounds(640, 135, 220, 25);
         mobiliaLabel.setForeground(Colors.TEXT_COLOR);
         mobiliaLabel.setFont(new Font("Dialog", Font.PLAIN, 18));
 
-        mobiliaField.setBounds(460, 75, 400, 55);
+        mobiliaField.setBounds(640, 175, 220, 25);
         mobiliaField.setBackground(Colors.SECONDARY_COLOR);
         mobiliaField.setForeground(Colors.TEXT_COLOR);
         mobiliaField.setFont(new Font("Dialog", Font.PLAIN, 24));
@@ -117,51 +123,51 @@ public class KitnetEditPage extends MyFrame implements ActionListener {
         inquilinoAlocadoField.setFont(new Font("Dialog", Font.PLAIN, 24));*/
 
         JLabel estadoUsoLabel = new JLabel("Estado de Uso:");
-        estadoUsoLabel.setBounds(460, 145, 200, 25);
+        estadoUsoLabel.setBounds(640, 200, 400, 25);
         estadoUsoLabel.setForeground(Colors.TEXT_COLOR);
         estadoUsoLabel.setFont(new Font("Dialog", Font.PLAIN, 18));
 
-        estadoUsoField.setBounds(460, 175, 400, 55);
+        estadoUsoField.setBounds(640, 235, 220, 25);
         estadoUsoField.setBackground(Colors.SECONDARY_COLOR);
         estadoUsoField.setForeground(Colors.TEXT_COLOR);
         estadoUsoField.setFont(new Font("Dialog", Font.PLAIN, 24));
 
         JLabel cepLabel = new JLabel("CEP:");
-        cepLabel.setBounds(30, 345, 200, 25);
+        cepLabel.setBounds(410, 200, 220, 25);
         cepLabel.setForeground(Colors.TEXT_COLOR);
         cepLabel.setFont(new Font("Dialog", Font.PLAIN, 18));
 
-        cepField.setBounds(30, 375, 400, 55);
+        cepField.setBounds(410, 235, 220, 25);
         cepField.setBackground(Colors.SECONDARY_COLOR);
         cepField.setForeground(Colors.TEXT_COLOR);
         cepField.setFont(new Font("Dialog", Font.PLAIN, 24));
 
         JLabel estadoLabel = new JLabel("Estado:");
-        estadoLabel.setBounds(460, 345, 200, 25);
+        estadoLabel.setBounds(640, 265, 400, 25);
         estadoLabel.setForeground(Colors.TEXT_COLOR);
         estadoLabel.setFont(new Font("Dialog", Font.PLAIN, 18));
 
-        estadoField.setBounds(460, 375, 400, 55);
+        estadoField.setBounds(640, 295, 220, 25);
         estadoField.setBackground(Colors.SECONDARY_COLOR);
         estadoField.setForeground(Colors.TEXT_COLOR);
         estadoField.setFont(new Font("Dialog", Font.PLAIN, 24));
 
         JLabel cidadeLabel = new JLabel("Cidade:");
-        cidadeLabel.setBounds(30, 445, 200, 25);
+        cidadeLabel.setBounds(410, 265, 400, 25);
         cidadeLabel.setForeground(Colors.TEXT_COLOR);
         cidadeLabel.setFont(new Font("Dialog", Font.PLAIN, 18));
 
-        cidadeField.setBounds(30, 475, 400, 55);
+        cidadeField.setBounds(410, 295, 220, 25);
         cidadeField.setBackground(Colors.SECONDARY_COLOR);
         cidadeField.setForeground(Colors.TEXT_COLOR);
         cidadeField.setFont(new Font("Dialog", Font.PLAIN, 24));
 
         JLabel enderecoLabel = new JLabel("Endereço:");
-        enderecoLabel.setBounds(460, 445, 200, 25);
+        enderecoLabel.setBounds(410, 330, 220, 25);
         enderecoLabel.setForeground(Colors.TEXT_COLOR);
         enderecoLabel.setFont(new Font("Dialog", Font.PLAIN, 18));
 
-        enderecoField.setBounds(460, 475, 400, 55);
+        enderecoField.setBounds(410, 360, 220, 25);
         enderecoField.setBackground(Colors.SECONDARY_COLOR);
         enderecoField.setForeground(Colors.TEXT_COLOR);
         enderecoField.setFont(new Font("Dialog", Font.PLAIN, 24));
@@ -170,7 +176,7 @@ public class KitnetEditPage extends MyFrame implements ActionListener {
         updateButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         updateButton.setBackground(Colors.TERTIARY_COLOR);
         updateButton.setForeground(Colors.SECONDARY_COLOR);
-        updateButton.setBounds(350, 550, 250, 50);
+        updateButton.setBounds(500, 470, 250, 50);
 
         add(nKitnetLabel);
         add(nKitnetField);
